c416b847e1a509ef777e5445a7ffa3ffe62f4034
Kainanteh <Kainante@hotmail.com>
2023-05-26 11:58:00 +0200

    Ordenador Sala 1

---
+++ Assets/Scripts/Ordenador/LogicaOrdenadorSala1.cs
@@ -0,0 +1,122 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LogicaOrdenadorSala1 : MonoBehaviour
+{
+
+
+
+
+
+   public KeyCode interactionKey = KeyCode.E;
+    public float interactionRange = 2f;
+    public LayerMask interactionLayer;
+
+    private Camera mainCamera;
+
+    public Camera ordenadorCamera;
+
+    public FirstPersonMovement FirstPersonMovementScript;
+    public FirstPersonLook FirstPersonLookScript;
+
+    public bool JugadorEnOrdenador = false;
+
+    public bool coroutineEjecutandose = false;
+    public float tiempocoroutine = 2f;
+
+    public GameObject Texto;
+
+
+
+    public bool MisionCheck = false;
+
+    public LogicaBoton LogicaBotonScript;
+
+    private void Start()
+    {
+        mainCamera = Camera.main;
+    }
+
+    private void Update()
+    {
+
+        if(MisionCheck == false)
+        {
+
+            if (Input.GetKeyDown(interactionKey))
+            {
+                Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+                RaycastHit hit;
+
+                if (Physics.Raycast(ray, out hit, interactionRange, interactionLayer))
+                {
+                    if (hit.collider.gameObject == gameObject)
+                    {
+                        if(mainCamera.enabled == true)
+                        {
+                            mainCamera.enabled = false; 
+                            ordenadorCamera.enabled = true; 
+                            FirstPersonMovementScript.estatico=true;
+                            FirstPersonLookScript.estatico=true;
+                            JugadorEnOrdenador = true;
+
+                        }
+                        else
+                        {
+                            mainCamera.enabled = true; 
+                            ordenadorCamera.enabled = false; 
+                            FirstPersonMovementScript.estatico=false;
+                            FirstPersonLookScript.estatico=false;
+                            JugadorEnOrdenador = false;
+                        }
+                    }
+                }
+            }
+
+            if(JugadorEnOrdenador == true)
+            {
+    
+                if (coroutineEjecutandose == false)
+                {
+                    StartCoroutine(ActivarDesactivarGameObject(Texto));
+                }
+
+            }
+
+        }
+        else
+        {
+       
+                        mainCamera.enabled = true; 
+                        ordenadorCamera.enabled = false; 
+                        FirstPersonMovementScript.estatico=false;
+                        FirstPersonLookScript.estatico=false;
+                        JugadorEnOrdenador = false;
+        }
+
+   
+        
+    }
+
+
+
+ 
+
+
+
+
+    private System.Collections.IEnumerator ActivarDesactivarGameObject(GameObject objeto)
+    {
+        coroutineEjecutandose = true;
+
+            objeto.SetActive(false);
+            yield return new WaitForSeconds(tiempocoroutine);
+            objeto.SetActive(true);
+            yield return new WaitForSeconds(tiempocoroutine);
+
+        coroutineEjecutandose = false;
+        
+        
+    }
+}
